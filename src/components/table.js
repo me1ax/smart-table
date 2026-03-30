352775730bc8f9be3f4ff8d270d9b107c1a4030c
Alexandr <melirina12345@gmail.com>
2026-03-30 18:35:20 +0400

    Попытка 1

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,41 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    [...before].reverse().forEach(id => {
+        root[id] = cloneTemplate(id);
+        root.container.prepend(root[id].container);
+    });
+
+    after.forEach(id => {
+        root[id] = cloneTemplate(id);
+        root.container.append(root[id].container);
+    })
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', (event) => {
+        onAction();
+    })
+
+    root.container.addEventListener('reset', (event) => {
+        setTimeout(onAction);
+    })
+
+    root.container.addEventListener('submit', (event) =>{
+        event.preventDefault();
+        onAction(event.submitter);
+    })
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => { 
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => { 
+                if(key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            })
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
