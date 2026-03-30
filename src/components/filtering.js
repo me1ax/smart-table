39d3a91bcb9858b63ffd238e39b79dd57f5a5b61
Alexandr <melirina12345@gmail.com>
2026-03-30 18:50:08 +0400

    Попытка 1

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -5,17 +5,20 @@ const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                        // Перебираем по именам
-        elements[elementName].append(                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                      .map(name => {                        // используйте name как значение и текстовое содержимое
-                        const opt = document.createElement('option');// @todo: создать и вернуть тег опции
-                        opt.value = name;
-                        opt.textContent  = name;
-                        return opt;
-                      })
-        )
+    Object.keys(indexes)
+      .forEach((elementName) => {
+        // Проверяем, существует ли элемент
+        if (elements[elementName]) {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName])
+                          .map(name => {
+                            const opt = document.createElement('option');
+                            opt.value = name;
+                            opt.textContent = name;
+                            return opt;
+                          })
+            )
+        }
      })
 
     return (data, state, action) => {
