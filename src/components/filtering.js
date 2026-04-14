62c8461d6a02d9c5e94677ed2571d1e996faa4a1
Alexandr <melirina12345@gmail.com>
2026-04-14 18:30:20 +0400

    Попытка 2

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,46 +1,44 @@
 export function initFiltering(elements) {
-  const updateIndexes = (elements, indexes) => {
-      Object.keys(indexes).forEach((elementName) => {
-          // Проверяем, существует ли элемент
-          if (elements[elementName]) {
-              // Очищаем select перед добавлением новых опций
-              elements[elementName].innerHTML = '';
-              elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
-                  const el = document.createElement('option');
-                  el.textContent = name;
-                  el.value = name;
-                  return el;
-              }));
-          }
-      });
-  };
-
-  const applyFiltering = (query, state, action) => {
-      // Обработка очистки полей
-      if (action && action.name === 'clear') {
-          Object.keys(elements).forEach(key => {
-              if (elements[key]) {
-                  elements[key].value = '';
-              }
-          });
-      }
-
-      // Формируем объект фильтрации для сервера
-      const filter = {};
-      Object.keys(elements).forEach(key => {
-          if (elements[key]) {
-              if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
-                  filter[`filter[${elements[key].name}]`] = elements[key].value;
-              }
-          }
-      });
-
-      // Если есть фильтры, добавляем их к query
-      return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
-  };
-
-  return {
-      updateIndexes,
-      applyFiltering
-  };
-}
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            // Проверяем, существует ли элемент
+            if (elements[elementName]) {
+                // Очищаем select перед добавлением новых опций
+                elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                    const el = document.createElement('option');
+                    el.textContent = name;
+                    el.value = name;
+                    return el;
+                }));
+            }
+        });
+    };
+  
+    const applyFiltering = (query, state, action) => {
+        // Обработка очистки полей
+        if (action && action.name === 'clear') {
+            const input = action.parentElement.querySelector('input, select');
+            if (input) {
+                input.value = '';
+            }
+        }
+  
+        // Формируем объект фильтрации для сервера
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
+                    filter[`filter[${elements[key].name}]`] = elements[key].value;
+                }
+            }
+        });
+  
+        // Если есть фильтры, добавляем их к query
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+    };
+  
+    return {
+        updateIndexes,
+        applyFiltering
+    };
+  }
